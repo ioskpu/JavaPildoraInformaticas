dd3266a9d23fadc90e754f43cf07db280e653365
ioskpu <lrcorales@gmail.com>
2024-09-05 17:30:50 -0500

    Declaraciones y Operadores
    clase de declaraciones y operadores en java

---
+++ Ejercicios/src/Declariones_Operadores.java
@@ -0,0 +1,27 @@
+
+public class Declariones_Operadores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 5;
+		
+		int b;
+		
+		b = 10;
+		
+		int c = a+b;
+		
+		c ++; //incremento en uno el valor de la variable
+		
+		System.out.println("la suma de las variables es: " + c);
+		
+		//incrementar el valor de la variable en un numero dado
+		
+		c += 4;
+		
+		System.out.println("ahora el valor de la variable c a cambiado por el incremento pasado en la variable: " + c);
+
+	}
+
+}
