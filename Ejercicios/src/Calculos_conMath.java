2ded600c07429081a040805b642a646976bf005b
ioskpu <lrcorales@gmail.com>
2024-09-06 09:08:42 -0500

    Estructuras principales clase Math
    ejemplo con Math.sqrt para obtener la raíz cuadrada de un numero

---
+++ Ejercicios/src/Calculos_conMath.java
@@ -0,0 +1,12 @@
+
+public class Calculos_conMath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double raiz = Math.sqrt(20);
+		
+		System.out.println("la raiz cuadrada de 20 es: " + raiz);
+	}
+
+}
